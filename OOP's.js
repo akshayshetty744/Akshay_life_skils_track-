d556b15d78b389f3760f042bba8d73f99220908d
Akshay Shetty <84218281+akshayshetty744@users.noreply.github.com>
2022-09-29 08:48:58 +0530

    Update OOP's.js

--- OOP's.js
+++ OOP's.js
@@ -33,6 +33,22 @@
     console.log(v);
 //--------------------------------------------------------------------------------------------
 //Abstraction
+class person{
+    constructor(name,id){
+        this.name = name;
+        this.id = id;
+    }
+    add_Address(add){
+        this.add = add;
+    }
+    getDetails(){
+        console.log(`Name is ${this.name},Address is: ${this.add}`);
+    }
+}
+ 
+let person1 = new person('Mukul',21);
+person1.add_Address('Delhi');
+person1.getDetails();
     function Vehicle()  
     {  
         this.vehicleName= vehicleName;  
